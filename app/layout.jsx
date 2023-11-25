c7b02d4026a4116711308b04523e53b3962c40c8
Guilherme Duccini <guilhermeduccini@gmail.com>
2023-11-25 14:38:56 -0300

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+import { Rubik } from "next/font/google";
+
+// Components
+import Navbar from "./components/Navbar";
+
+const rubik = Rubik({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Dojo Helpdesk",
+  description: "Duccini Front-end Developer",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={rubik.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
